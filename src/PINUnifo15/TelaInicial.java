21a3ad3c70cbf61f017c23dd2ef374cb613eb8a8
Matheus Maia <contato@matheusmaia.com>
2015-10-12 15:24:51 -0300

    Imagens

--- src/PINUnifo15/TelaInicial.java
+++ src/PINUnifo15/TelaInicial.java
@@ -27,8 +27,6 @@ import javax.swing.UIManager;
 public class TelaInicial extends JFrame {
 
 	private JPanel contentPane;
-	private JDesktopPane desktopPane;
-	private JButton btnNewButton;
 	private Form form1;
 
 	/**
@@ -69,75 +67,108 @@ public class TelaInicial extends JFrame {
 		menuBar.add(mnArquivo);
 		
 		JMenu mnSair = new JMenu("Sair");
-		mnSair.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/001_29.png")));
+		mnSair.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Exit.png")));
 		mnArquivo.add(mnSair);
 		
-		JMenu mnEditar = new JMenu("Editar");
-		menuBar.add(mnEditar);
+		JMenu mnCadastrar = new JMenu("Cadastrar");
+		menuBar.add(mnCadastrar);
+		
+		JMenu mnUsurio = new JMenu("Usu\u00E1rio");
+		mnUsurio.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/People.png")));
+		mnCadastrar.add(mnUsurio);
+		
+		JMenu mnNovoUsuario = new JMenu("Novo usuario");
+		mnNovoUsuario.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Person.png")));
+		mnUsurio.add(mnNovoUsuario);
+		
+		JMenu mnEditarUsurio = new JMenu("Editar usu\u00E1rio");
+		mnEditarUsurio.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Redo.png")));
+		mnUsurio.add(mnEditarUsurio);
+		
+		JMenu mnExcluirUsurio = new JMenu("Excluir usu\u00E1rio");
+		mnExcluirUsurio.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Delete.png")));
+		mnUsurio.add(mnExcluirUsurio);
+		
+		JMenu mnEvento = new JMenu("Evento");
+		mnEvento.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Calendar.png")));
+		mnCadastrar.add(mnEvento);
+		
+		JMenu mnNewMenu_1 = new JMenu("Novo");
+		mnNewMenu_1.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Create.png")));
+		mnEvento.add(mnNewMenu_1);
+		
+		JMenu mnNewMenu_2 = new JMenu("Editar");
+		mnNewMenu_2.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Redo.png")));
+		mnEvento.add(mnNewMenu_2);
+		
+		JMenu mnExcluir_1 = new JMenu("Excluir");
+		mnExcluir_1.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Delete.png")));
+		mnEvento.add(mnExcluir_1);
+		
+		JMenu mnEstabelecimento = new JMenu("Estabelecimento");
+		mnEstabelecimento.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Company.png")));
+		mnCadastrar.add(mnEstabelecimento);
+		
+		JMenu mnNovo = new JMenu("Novo");
+		mnNovo.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Create.png")));
+		mnEstabelecimento.add(mnNovo);
+		
+		JMenu mnEditar_1 = new JMenu("Editar");
+		mnEditar_1.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Redo.png")));
+		mnEstabelecimento.add(mnEditar_1);
+		
+		JMenu mnExcluir = new JMenu("Excluir");
+		mnExcluir.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Delete.png")));
+		mnEstabelecimento.add(mnExcluir);
+		
+		JMenu mnRelatrios = new JMenu("Relat\u00F3rios");
+		menuBar.add(mnRelatrios);
+		
+		JMenu mnNewMenu_3 = new JMenu("Usu\u00E1rios");
+		mnNewMenu_3.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/People.png")));
+		mnRelatrios.add(mnNewMenu_3);
+		
+		JMenu mnUsuriosAtivos = new JMenu("Usu\u00E1rios ativos");
+		mnNewMenu_3.add(mnUsuriosAtivos);
+		
+		JMenu mnNewMenu_4 = new JMenu("Estabelecimento");
+		mnNewMenu_4.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Company.png")));
+		mnRelatrios.add(mnNewMenu_4);
+		
+		JMenu mnTodosEstabelecimentos = new JMenu("Todos estabelecimentos");
+		mnNewMenu_4.add(mnTodosEstabelecimentos);
+		
+		JMenu mnNewMenu_5 = new JMenu("Eventos");
+		mnNewMenu_5.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Calendar.png")));
+		mnRelatrios.add(mnNewMenu_5);
+		
+		JMenu mnMediaDeVisitante = new JMenu("Media de visitante");
+		mnNewMenu_5.add(mnMediaDeVisitante);
 		
 		JMenu mnSobre = new JMenu("Sobre");
 		menuBar.add(mnSobre);
 		
 		JMenu mnUniforOnline = new JMenu("Unifor Online");
-		mnUniforOnline.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/001_40.png")));
 		mnSobre.add(mnUniforOnline);
 		
 		JMenu mnNewMenu = new JMenu("Vers\u00E3o do programa");
-		mnNewMenu.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/001_33.png")));
 		mnSobre.add(mnNewMenu);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
-		JToolBar toolBar = new JToolBar();
-		
-		desktopPane = new JDesktopPane();
-		desktopPane.setBackground(UIManager.getColor("CheckBox.background"));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addComponent(toolBar, GroupLayout.DEFAULT_SIZE, 894, Short.MAX_VALUE)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(10)
-					.addComponent(desktopPane)
-					.addGap(10))
+				.addGap(0, 894, Short.MAX_VALUE)
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(toolBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(desktopPane)
-					.addContainerGap())
+				.addGap(0, 530, Short.MAX_VALUE)
 		);
-		GroupLayout gl_desktopPane = new GroupLayout(desktopPane);
-		gl_desktopPane.setHorizontalGroup(
-			gl_desktopPane.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 874, Short.MAX_VALUE)
-		);
-		gl_desktopPane.setVerticalGroup(
-			gl_desktopPane.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 468, Short.MAX_VALUE)
-		);
-		desktopPane.setLayout(gl_desktopPane);
-		
-		btnNewButton = new JButton("New button");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		
-		btnNewButton.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/001_54.png")));
-		toolBar.add(btnNewButton);
 		contentPane.setLayout(gl_contentPane);
 	}
 	
 	private void createEvents(){
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				// Parei nessa parte do vídeo https://youtu.be/TUL3qEoZkR4?list=PLOWSuMndobuYQPttmTBByksBWtONmxPA1&t=1659
-			}
-		});
 	}
 }
