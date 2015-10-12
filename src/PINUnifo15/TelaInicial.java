ef84f042932cddc097f2bfd729b78edc226fb223
Matheus Maia <contato@matheusmaia.com>
2015-10-12 12:27:50 -0300

    Foi criado o modo de visualização das telas.

--- src/PINUnifo15/TelaInicial.java
+++ src/PINUnifo15/TelaInicial.java
@@ -11,6 +11,7 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JMenuItem;
 import java.awt.event.ActionListener;
+import java.text.Normalizer.Form;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -20,10 +21,15 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JToolBar;
 import javax.swing.JMenu;
 import javax.swing.ImageIcon;
+import javax.swing.JDesktopPane;
+import javax.swing.UIManager;
 
 public class TelaInicial extends JFrame {
 
 	private JPanel contentPane;
+	private JDesktopPane desktopPane;
+	private JButton btnNewButton;
+	private Form form1;
 
 	/**
 	 * Launch the application.
@@ -45,6 +51,11 @@ public class TelaInicial extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaInicial() {
+		initComponentes();
+		createEvents();
+	}
+	
+	private void initComponentes(){
 		setIconImage(Toolkit.getDefaultToolkit().getImage(TelaInicial.class.getResource("/img/UNIFOR_Logo_21.gif")));
 		setBackground(Color.WHITE);
 		setTitle("UniforOnline - Para desktops");
@@ -58,7 +69,7 @@ public class TelaInicial extends JFrame {
 		menuBar.add(mnArquivo);
 		
 		JMenu mnSair = new JMenu("Sair");
-		mnSair.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/001_39.png")));
+		mnSair.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/001_29.png")));
 		mnArquivo.add(mnSair);
 		
 		JMenu mnEditar = new JMenu("Editar");
@@ -78,15 +89,55 @@ public class TelaInicial extends JFrame {
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		
+		JToolBar toolBar = new JToolBar();
+		
+		desktopPane = new JDesktopPane();
+		desktopPane.setBackground(UIManager.getColor("CheckBox.background"));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 894, Short.MAX_VALUE)
+				.addComponent(toolBar, GroupLayout.DEFAULT_SIZE, 894, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(10)
+					.addComponent(desktopPane)
+					.addGap(10))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 530, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(toolBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(desktopPane)
+					.addContainerGap())
 		);
+		GroupLayout gl_desktopPane = new GroupLayout(desktopPane);
+		gl_desktopPane.setHorizontalGroup(
+			gl_desktopPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 874, Short.MAX_VALUE)
+		);
+		gl_desktopPane.setVerticalGroup(
+			gl_desktopPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 468, Short.MAX_VALUE)
+		);
+		desktopPane.setLayout(gl_desktopPane);
+		
+		btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		btnNewButton.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/001_54.png")));
+		toolBar.add(btnNewButton);
 		contentPane.setLayout(gl_contentPane);
+	}
+	
+	private void createEvents(){
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Parei nessa parte do vídeo https://youtu.be/TUL3qEoZkR4?list=PLOWSuMndobuYQPttmTBByksBWtONmxPA1&t=1659
+			}
+		});
 	}
 }
